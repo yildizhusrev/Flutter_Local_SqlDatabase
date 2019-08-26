2c3b8953abcf77a9fa77a32393fcd735121f0b27
hüsrev yıldız <husrev_yildiz@hotmail.com>
2019-08-26 14:09:25 +0300

    alert_dialog_and_cgoose

--- flutter_sqldatabase/lib/screens/wordDetail.dart
+++ flutter_sqldatabase/lib/screens/wordDetail.dart
@@ -11,6 +11,8 @@ class WordDetail extends StatefulWidget {
   State<StatefulWidget> createState() => WordDetailState(word);
 }
 
+enum Choise{Delete,Update}
+
 class WordDetailState extends State {
   Word word;
   WordDetailState(this.word);
@@ -21,6 +23,28 @@ class WordDetailState extends State {
     return Scaffold(
         appBar: AppBar(
           title: Text("Kelime Detayları"),
+          actions: <Widget>[
+            PopupMenuButton<Choise>(
+              onSelected: (Choise choise){
+                AlertDialog alert = AlertDialog(
+                  title: Text("Seçim"),
+                  content: Text(choise.toString()),
+                );
+                showDialog(context: context,builder: (_)=>alert);
+              },
+              itemBuilder: (BuildContext context)=><PopupMenuEntry<Choise>>[
+                PopupMenuItem<Choise>(
+                  value: Choise.Delete,
+                  child: Text("Delete Word"),
+                ),
+                 PopupMenuItem<Choise>(
+                  value: Choise.Update,
+                  child: Text("Update Word"),
+                )
+
+              ],
+            )
+          ],
         ),
         body: ListView.separated(
           padding: const EdgeInsets.all(8.0),
@@ -29,6 +53,7 @@ class WordDetailState extends State {
             return Container(
               height: 50,
               color: colorCodes[index],
+              
               child: Center(child: Text('${entries[index]}')),              
             );
           },
